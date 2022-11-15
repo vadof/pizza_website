78b82ad8013bd1378887c6f085b35debdbe0a781
Vadim Filonov <vafilo@ttu.ee>
2022-11-15 11:55:31 +0200

    Do urls for menu

--- iaib_pizza/pizza/urls.py
+++ iaib_pizza/pizza/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home')
+    path('', index, name='home'),
+    path('menu/pizza/', pizza_menu, name='pizza'),
+    path('menu/burger/', burger_menu, name='burger'),
+    path('menu/drinks/', drinks_menu, name='drinks'),
 ]
--- iaib_pizza/pizza/views.py
+++ iaib_pizza/pizza/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'pizza/index.html')
+
+def pizza_menu(request):
+    return render(request, 'pizza/menu/pizza.html')
+
+def burger_menu(request):
+    return render(request, 'pizza/menu/burger.html')
+
+def drinks_menu(request):
+    return render(request, 'pizza/menu/drinks.html')
