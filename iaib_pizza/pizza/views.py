121329131f6644156f36ee93f4844baa194c19c2
Vadim Filonov <vafilo@ttu.ee>
2022-11-22 14:51:09 +0200

    create pizza menu template

--- iaib_pizza/pizza/views.py
+++ iaib_pizza/pizza/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from pizza.menu_storage.pizza_storage import pizzas
+
 
 def index(request):
     return render(request, 'pizza/index.html')
 
 def pizza_menu(request):
-    return render(request, 'pizza/menu/pizza.html')
+    return render(request, 'pizza/menu/pizza.html', {'pizzas': pizzas, 'title': 'Pizza menu'})
 
 def burger_menu(request):
     return render(request, 'pizza/menu/burger.html')
