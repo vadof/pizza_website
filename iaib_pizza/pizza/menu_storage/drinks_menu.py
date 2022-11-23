4a4ca41c3484167d199122c2c8d62ba978482bdc
aetae <117419306+aetae@users.noreply.github.com>
2022-11-23 16:46:02 +0200

    added some drinks, some burgers

--- iaib_pizza/pizza/menu_storage/drinks_menu.py
+++ iaib_pizza/pizza/menu_storage/drinks_menu.py
@@ -1,5 +1,8 @@
 from .menu import Drink
 
-cola = Drink('Coca Cola', 1.90, 333, 'cola.jpg', 1)
+cola = Drink('Coca Cola', 1.90, 333, 'coca.jpg', 1)
+fanta = Drink('Fanta', 1.90, 333, 'fanta.jpg', 2)
+sprite = Drink('Sprite', 1.90, 333, 'sprite.jpg', 3)
+ice_tea = Drink('Ice Tea', 1.90, 333, 'ice_tea.jpg', 4)
 
-drinks = [cola]
+drinks = [cola, fanta, sprite, ice_tea]
