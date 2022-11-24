6ba3d939a7773d77c9ef80bd06581eb79b390563
Vadim Filonov <vafilo@ttu.ee>
2022-11-24 12:42:10 +0200

    change id

--- iaib_pizza/pizza/menu_storage/drinks_menu.py
+++ iaib_pizza/pizza/menu_storage/drinks_menu.py
@@ -1,8 +1,8 @@
 from .menu import Drink
 
-cola = Drink('Coca Cola', 1.90, 333, 'coca.jpg', 1)
-fanta = Drink('Fanta', 1.90, 333, 'fanta.jpg', 2)
-sprite = Drink('Sprite', 1.90, 333, 'sprite.jpg', 3)
-ice_tea = Drink('Ice Tea', 1.90, 333, 'ice_tea.jpg', 4)
+cola = Drink('Coca Cola', 1.90, 333, 'coca.jpg', 41)
+fanta = Drink('Fanta', 1.90, 333, 'fanta.jpg', 42)
+sprite = Drink('Sprite', 1.90, 333, 'sprite.jpg', 43)
+ice_tea = Drink('Ice Tea', 1.90, 333, 'ice_tea.jpg', 44)
 
 drinks = [cola, fanta, sprite, ice_tea]
