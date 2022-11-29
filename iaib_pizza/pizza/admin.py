7600d2ee225ae9b8f3443d00a16db93052968a28
Vadim Filonov <vafilo@ttu.ee>
2022-11-29 15:08:07 +0200

    create admin panel, deleted menu storage

--- iaib_pizza/pizza/admin.py
+++ iaib_pizza/pizza/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Pizza, Burger, Drink, Category
 
-# Register your models here.
+
+class Pizza_Burger_Admin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'ingredients', 'available')
+    list_display_links = ('name',)
+    search_fields = ('name',)
+    list_editable = ('available',)
+
+
+class Drink_Admin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'ml', 'available')
+    list_display_links = ('name',)
+    search_fields = ('name',)
+    list_editable = ('available',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name')
+
+
+admin.site.register(Pizza, Pizza_Burger_Admin)
+admin.site.register(Burger, Pizza_Burger_Admin)
+admin.site.register(Drink, Drink_Admin)
+
+admin.site.register(Category, CategoryAdmin)
--- iaib_pizza/pizza/views.py
+++ iaib_pizza/pizza/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render
 
-from pizza.menu_storage.pizza_menu import pizzas
-from pizza.menu_storage.burger_menu import burgers
-from pizza.menu_storage.drinks_menu import drinks
+from .models import Pizza, Burger, Drink
 
 
 def index(request):
     return render(request, 'pizza/index.html')
 
 def pizza_menu(request):
-    return render(request, 'pizza/menu/pizza.html', {'pizzas': pizzas, 'title': 'Pizza menu'})
+    menu = Pizza.objects.all()
+    return render(request, 'pizza/menu/pizza.html', {'menu': menu, 'title': 'Pizza menu'})
 
 def burger_menu(request):
-    return render(request, 'pizza/menu/burger.html', {'burgers': burgers, 'title': 'Burger menu'})
+    menu = Burger.objects.all()
+    return render(request, 'pizza/menu/burger.html', {'menu': menu, 'title': 'Burger menu'})
 
 def drinks_menu(request):
-    return render(request, 'pizza/menu/drinks.html', {'drinks': drinks, 'title': 'Drinks menu'})
+    menu = Drink.objects.all()
+    return render(request, 'pizza/menu/drinks.html', {'menu': menu, 'title': 'Drinks menu'})
 
 def cart(request):
     return render(request, 'pizza/cart.html', {'title': 'Cart'})
