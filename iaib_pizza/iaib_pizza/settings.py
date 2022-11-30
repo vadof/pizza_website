fdb9f11c23263f1d7a18272e60fa130f6450cc4e
Vadim Filonov <vafilo@ttu.ee>
2022-11-30 16:59:18 +0200

    change db

--- iaib_pizza/iaib_pizza/settings.py
+++ iaib_pizza/iaib_pizza/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pizza.apps.PizzaConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
--- iaib_pizza/pizza/views.py
+++ iaib_pizza/pizza/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render
 
-from .models import Pizza, Burger, Drink
+from .models import Product
 
 
 def index(request):
     return render(request, 'pizza/index.html')
 
 def pizza_menu(request):
-    menu = Pizza.objects.all()
+    menu = [product for product in Product.objects.all() if product.category_id == 1]
+    print(menu)
     return render(request, 'pizza/menu/pizza.html', {'menu': menu, 'title': 'Pizza menu'})
 
 def burger_menu(request):
-    menu = Burger.objects.all()
+    menu = [product for product in Product.objects.all() if product.category_id == 2]
     return render(request, 'pizza/menu/burger.html', {'menu': menu, 'title': 'Burger menu'})
 
 def drinks_menu(request):
-    menu = Drink.objects.all()
+    menu = [product for product in Product.objects.all() if product.category_id == 3]
     return render(request, 'pizza/menu/drinks.html', {'menu': menu, 'title': 'Drinks menu'})
 
 def cart(request):
