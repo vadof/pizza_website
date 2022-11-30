b40ff9faff35efdea3cdaaa42de6e01f5a961977
Vadim Filonov <vafilo@ttu.ee>
2022-11-30 19:29:03 +0200

    trying to do cart

--- iaib_pizza/cart/cart.py
+++ iaib_pizza/cart/cart.py
@@ -1,5 +1,5 @@
+from decimal import Decimal
 from django.conf import settings
-
 from pizza.models import Product
 
 
@@ -12,11 +12,27 @@ class Cart(object):
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
+    def __iter__(self):
+        product_ids = self.cart.keys()
+
+        products = Product.objects.filter(id__in=product_ids)
+
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
+
     def add(self, product, quantity=1, update_quantity=False):
         product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
-
         if update_quantity:
             self.cart[product_id]['quantity'] = quantity
         else:
@@ -32,6 +48,12 @@ class Cart(object):
             del self.cart[product_id]
             self.save()
 
-    def __iter__(self):
-        product_ids = self.cart.keys()
-        products = Product.objects.filter(id__in=product_ids)
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+
+    def get_total_price_with_delivery(self):
+        return self.get_total_price() + 2
+
+    def clear(self):
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
--- iaib_pizza/pizza/views.py
+++ iaib_pizza/pizza/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from .models import Product
+from .models import Product, Category
+from cart.forms import CartAddProductForm
 
 
 def index(request):
@@ -8,7 +9,6 @@ def index(request):
 
 def pizza_menu(request):
     menu = [product for product in Product.objects.all() if product.category_id == 1]
-    print(menu)
     return render(request, 'pizza/menu/pizza.html', {'menu': menu, 'title': 'Pizza menu'})
 
 def burger_menu(request):
@@ -20,4 +20,20 @@ def drinks_menu(request):
     return render(request, 'pizza/menu/drinks.html', {'menu': menu, 'title': 'Drinks menu'})
 
 def cart(request):
-    return render(request, 'pizza/cart.html', {'title': 'Cart'})
+    return render(request, 'iaib_pizza/cart/detail.html', {'title': 'Cart'})
+
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request, 'shop/product/list.html', {'category': category, 'categories': categories, 'products': products})
+
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id, available=True)
+    cart_product_form = CartAddProductForm()
+    return render(request, 'iaib_pizza/cart/detail.html', {'product': product, 'cart_product_form': cart_product_form})
