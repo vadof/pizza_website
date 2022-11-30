dae177d952d55d159141310dc32423cfe41ffd0f
Vadim Filonov <vafilo@ttu.ee>
2022-11-30 15:13:51 +0200

    add cart session, fix db id, deleted unused module

--- iaib_pizza/iaib_pizza/urls.py
+++ iaib_pizza/iaib_pizza/urls.py
@@ -19,7 +19,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from pizza.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
