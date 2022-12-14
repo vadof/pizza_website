67f5a18132078c93d288da64710b28c7ea461f1e
Vadim Filonov <vafilo@ttu.ee>
2022-12-14 16:38:30 +0200

    added db for checkout button

--- iaib_pizza/cart/models.py
+++ iaib_pizza/cart/models.py
@@ -3,31 +3,34 @@ import datetime
 from django.db import models
 
 class Sales(models.Model):
-    product_id = models.CharField(max_length=3)
+    product_id = models.IntegerField()
     sold = models.IntegerField()
     income = models.DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
         verbose_name_plural = 'Sales'
+        ordering = ['product_id']
 
 
 class DiscountSales(models.Model):
-    product_id = models.CharField(max_length=3)
+    product_id = models.IntegerField()
     sold = models.IntegerField()
     discount = models.IntegerField()
     income = models.DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
         verbose_name_plural = 'Discount Sales'
+        ordering = ['product_id', 'discount']
 
 
 class SalesWithoutDiscount(models.Model):
-    product_id = models.CharField(max_length=3)
+    product_id = models.IntegerField()
     sold = models.IntegerField()
     income = models.DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
         verbose_name_plural = 'Sales Without Discount'
+        ordering = ['product_id']
 
 class OrderInfo(models.Model):
     name = models.CharField(max_length=100)
@@ -35,11 +38,11 @@ class OrderInfo(models.Model):
     city = models.CharField(max_length=100)
     zip = models.IntegerField()
     phone_number = models.IntegerField()
-    discount = models.IntegerField(blank=True)
+    discount = models.IntegerField(blank=True, null=True)
     products = models.TextField(max_length=300)
-    time = models.DateTimeField(auto_now=True)
+    amount = models.TextField(max_length=300)
+    time = models.TimeField(auto_now_add=True)
     date = models.DateField(auto_now=True)
-    delivery = models.BooleanField()
 
     class Meta:
         verbose_name_plural = 'Order Info'
